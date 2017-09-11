deaabeb3e1d97cec1856e310e42448d88d1b8f36
Dennis Samuel Sheehan <ssheehan1210@yahoo.com>
2017-09-11 06:05:13 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+/* console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree); */
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardOne = cards[0];
+
+var cardsInPlay = [];
+
+cardsInPlay.push(cardOne);
+
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
