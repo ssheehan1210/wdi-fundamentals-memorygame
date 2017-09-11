a491696d2d792efc83d95673ecd5cffbe602bcbe
Dennis Samuel Sheehan <ssheehan1210@yahoo.com>
2017-09-11 10:55:36 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/images/queen-of-hearts.png"
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/images/queen-of-diamonds.png"
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/images/king-of-hearts.png"
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
@@ -13,8 +34,10 @@ var checkForMatch = function () {
 };
 
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
